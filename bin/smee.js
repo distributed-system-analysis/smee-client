f57f39eb038474083256a8ad3bf95d45e9f41295
Peter Portante <peter.portante@redhat.com>
2023-04-13 00:13:32 -0400

    Make command line handling testable

--- bin/smee.js
+++ bin/smee.js
@@ -1,30 +1,37 @@
 #!/usr/bin/env node
 
-const program = require('commander')
+const { Command } = require('commander')
 const { version } = require('../package.json')
 const Client = require('../client.js')
 
-program
-  .version(version, '-v, --version')
-  .usage('[options]')
-  .option('-u, --url <url>', 'URL of the webhook proxy service. Default: https://smee.io/new')
-  .option('-t, --target <target>', 'Full URL (including protocol and path) of the target service the events will forwarded to. Default: http://127.0.0.1:PORT/PATH')
-  .option('-p, --port <n>', 'Local HTTP server port', process.env.PORT || 3000)
-  .option('-P, --path <path>', 'URL path to post proxied requests to`', '/')
-  .parse()
+function parseCommandLine (argv) {
+  const program = new Command()
 
-const options = program.opts()
+  program
+    .version(version, '-v, --version')
+    .usage('[options]')
+    .option('-u, --url <url>', 'URL of the webhook proxy service. Default: https://smee.io/new')
+    .option('-t, --target <target>', 'Full URL (including protocol and path) of the target service the events will forwarded to. Default: http://127.0.0.1:PORT/PATH')
+    .option('-p, --port <n>', 'Local HTTP server port', process.env.PORT || 3000)
+    .option('-P, --path <path>', 'URL path to post proxied requests to`', '/')
+    .parse(argv)
 
-let target
-if (options.target) {
-  target = options.target
-} else {
-  target = `http://127.0.0.1:${options.port}${options.path}`
+  const options = program.opts()
+
+  let target
+  if (options.target) {
+    target = options.target
+  } else {
+    target = `http://127.0.0.1:${options.port}${options.path}`
+  }
+
+  return {
+    source: options.url,
+    target
+  }
 }
 
-async function setup () {
-  let source = options.url
-
+async function runClient ({ source, target }) {
   if (!source) {
     source = await Client.createChannel()
   }
@@ -33,4 +40,5 @@ async function setup () {
   client.start()
 }
 
-setup()
+const args = parseCommandLine(process.argv)
+runClient(args)
