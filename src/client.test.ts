6cd54d975aec442e349bd911d2c89628d11bae0d
Peter Portante <peter.portante@redhat.com>
2023-04-15 20:43:34 -0400

    Add a test for the `Client.start()` method
    Refactor to not use nested describes since we are testing all Client
    methods.

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,40 +1,58 @@
 import Client = require('./client')
 import nock = require('nock')
+import EventSource from 'eventsource'
 
-describe('client', () => {
-  describe('Client', () => {
-    test('verify Client constructor', () => {
-      let src: string = 'http://this.example.com/'
-      let tgt: string = 'http://that.example.com'
-      let opts = { source: src, target: tgt }
+jest.mock('eventsource', () => {
+  return jest.fn().mockImplementation((urlArg) => {
+    return {
+      addEventListener: jest.fn(),
+      url: urlArg
+    }
+  })
+})
 
-      const client = new Client(opts)
+describe('Client', () => {
+  test('verify Client constructor behaviors', () => {
+    let src: string = 'http://this.example.com/'
+    let tgt: string = 'http://that.example.com'
+    let opts = { source: src, target: tgt }
 
-      expect(client.source).toBe(src)
-      expect(client.target).toBe(tgt)
+    const client = new Client(opts)
 
-      src = 'bad src url'
-      opts = { source: src, target: tgt }
+    expect(client.source).toBe(src)
+    expect(client.target).toBe(tgt)
 
-      expect(() => new Client(opts)).toThrow()
+    src = 'bad src url'
+    opts = { source: src, target: tgt }
 
-      src = 'http://good.example.com'
-      tgt = 'bad tgt url'
-      opts = { source: src, target: tgt }
+    expect(() => new Client(opts)).toThrow()
 
-      expect(() => new Client(opts)).toThrow()
-    })
+    src = 'http://good.example.com'
+    tgt = 'bad tgt url'
+    opts = { source: src, target: tgt }
+
+    expect(() => new Client(opts)).toThrow()
   })
 
-  describe('createChannel', () => {
-    test('returns a new channel', async () => {
-      const req = nock('https://smee.io').head('/new').reply(302, '', {
-        Location: 'https://smee.io/abc123'
-      })
+  test('verify .start() method behaviors', () => {
+    const src: string = 'http://src.example.com/'
+    const tgt: string = 'http://tgt.example.com'
+    const opts = { source: src, target: tgt }
 
-      const channel = await Client.createChannel()
-      expect(channel).toEqual('https://smee.io/abc123')
-      expect(req.isDone()).toBe(true)
+    const client = new Client(opts)
+    client.start()
+
+    expect(EventSource).toHaveBeenCalledTimes(1)
+    expect(client.events.addEventListener).toHaveBeenCalledTimes(3)
+  })
+
+  test('createChannel returns a new channel', async () => {
+    const req = nock('https://smee.io').head('/new').reply(302, '', {
+      Location: 'https://smee.io/abc123'
     })
+
+    const channel = await Client.createChannel()
+    expect(channel).toEqual('https://smee.io/abc123')
+    expect(req.isDone()).toBe(true)
   })
 })
