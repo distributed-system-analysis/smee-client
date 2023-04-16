843a487975f20a0f8a486c6a7fd43fefa98c9ef6
Peter Portante <peter.portante@redhat.com>
2023-04-15 20:46:21 -0400

    Add initial test for `Client.onmessage()`

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,6 +1,7 @@
 import Client = require('./client')
 import nock = require('nock')
 import EventSource from 'eventsource'
+import flushPromises = require('flush-promises')
 
 jest.mock('eventsource', () => {
   return jest.fn().mockImplementation((urlArg) => {
@@ -44,6 +45,30 @@ describe('Client', () => {
 
     expect(EventSource).toHaveBeenCalledTimes(1)
     expect(client.events.addEventListener).toHaveBeenCalledTimes(3)
+  })
+
+  test('verify .onmessage() method behaviors', async () => {
+    const srcHost: string = 'src.example.com'
+    const tgtHost: string = 'tgt.example.com'
+    const tgtUrl: string = `http://${tgtHost}/web-hook`
+
+    const req = nock(`http://${tgtHost}`).post('/web-hook').query({ foo: 'bar', then: 'now' }).reply(200, 'Posted')
+
+    // We "receive" messages from src/channel5 and post to ${tgtUrl}.
+    const opts = { source: `http://${srcHost}/channel5`, target: tgtUrl }
+    const client = new Client(opts)
+
+    // Message that will be "received".
+    const msg = { data: `{"query": {"foo": "bar", "then": "now"}, "host": "${srcHost}", "body": "payload", "key1": "one", "key2": "two"}` }
+
+    // "Receive" the message.
+    client.onmessage(msg)
+
+    // Ensure all POST operations complete before test ends.
+    await flushPromises()
+    await flushPromises()
+
+    expect(req.isDone()).toBe(true)
   })
 
   test('createChannel returns a new channel', async () => {
